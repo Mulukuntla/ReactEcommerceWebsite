cb3b8898fceb5c5593e551559c5bde6c9416c872
Mulukuntla <yours.saisaketh@gmail.com>
2025-07-29 12:01:25 +0530

    coded cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React from "react";
 import Header from "./components/Layout/Header"
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
-    <div>
+    <React.Fragment>
+      <Cart></Cart>
       <Header></Header>
       <main>
         <Meals></Meals>
       </main>
      
-    </div>
+    </React.Fragment>
   );
 }
 
